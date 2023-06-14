367db8c181433f5c59908007b775ca00af23d5c4
Linamohamed89 <linaaloqaily1989@gmail.com>
2023-06-14 18:44:36 +0200

    add new variables tasks file

---
+++ python variables tasks.py
@@ -0,0 +1,49 @@
+
+
+#Python Variables Tasks
+
+
+#1 Create a Boolean variable named x
+
+x=
+#2 Create an integer variable named y
+
+y=
+#3 Create a float variable named z
+
+z=
+#4 Create a string variable names s
+
+s=
+#5 Convert the int variable to float
+
+
+#6 Can we convert the str to int ?
+
+
+#7 Create a list of numbers from 1 to 5
+
+
+#8 Create a tuple from 10 to 15
+
+
+#9 Convert the list to tuple
+
+
+
+#10 Create a dict of 3 values
+
+
+#11 Can we use semi colon ; with python
+#no this is Illigal.
+
+
+#12 Python is interpreted or compiled ?
+#Python is interpreted.
+
+#13 What is the differences between low level &high level
+
+
+
+
+python file

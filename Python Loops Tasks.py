19d4e970b7714c577bdc39706fbc1f4d1d8b4860
Linamohamed89 <linaaloqaily1989@gmail.com>
2023-06-16 20:57:35 +0200

    add python loop tasks

---
+++ Python Loops Tasks.py
@@ -0,0 +1,50 @@
+
+
+#Python Loops Tasks
+
+
+#1. Print numbers from 0 to 10 using while
+
+x=0
+while 0>10:
+    print(x)
+    x +=1
+    
+#2. Print numbers from 0 to 1o using for
+
+
+for h in range (11):
+    print(h)
+    
+#3. Stop the loop if the number = 5
+
+
+x >= 5
+while x >= 5 : print(x);x +=1
+
+
+    
+#4. Skip the 5 iteration from print
+
+
+#5. Print multiplication table from 1 to 5
+#6. How to get numbers from 10 to 20 using range
+
+# Generate numbers between 10 to 20
+
+for i in range(10,20):
+    print(i)
+
+print ('----------------------')
+#7. How to get numbers from 10 to 100 with 3 at each step using range
+
+for i in range(10,100,3):
+    print(i)
+
+print ('----------------------')
+# Generate numbers between 10 to 100
+
+
+#8. Get a list of even numbers from 1 to 100 using for
+#9. Get a list of even numbers from 1 to 100 using while
+#10. Get a list of even numbers from 1 to 100 using range
